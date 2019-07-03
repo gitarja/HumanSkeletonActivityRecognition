e45ef365d18a326f5473832e8b5b4f8e196f1dd7
unknown <prasetiautama2@gmail.com>
2019-07-03 17:59:58 +0900

    Using return sequence

--- models/CFGRNN.py
+++ models/CFGRNN.py
@@ -67,7 +67,7 @@ class CFGRNN(K.models.Model):
 
         #self.classifier = K.layers.TimeDistributed(K.layers.Dense(units=conf["N_CLASS"], activation=None, name="equation"))
         self.classifier =  K.layers.Bidirectional(
-            K.layers.SimpleRNN(units=conf["classifier"]["units"], return_sequences=False, name="classifier"),
+            K.layers.SimpleRNN(units=conf["classifier"]["units"], return_sequences=True, name="classifier"),
             merge_mode="concat")
 
         self.concat = K.layers.Concatenate(axis=-1)
