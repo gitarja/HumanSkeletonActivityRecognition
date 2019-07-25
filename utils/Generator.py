132d57fef98c9536aa52b3724de36646ef2c60c6
unknown <prasetiautama2@gmail.com>
2019-07-25 17:17:59 +0900

    OpenPTrack Generator

--- utils/Generator.py
+++ utils/Generator.py
@@ -9,13 +9,18 @@ class Generator:
         self.batch_size = batch_size
         self.T = t
         self.train = train
-        self.data_set = pd.read_csv(dataset_path)
+        extension = dataset_path.split(".")[-1]
+        if extension == "csv":
+            self.data_set = pd.read_csv(dataset_path)
+        elif extension == "pkl":
+            self.data_set = pd.read_pickle(dataset_path)
         self.lb = preprocessing.LabelBinarizer()
         self.lb.fit(range(1, n_class + 1))
         #self.lb.fit([1, 2, 5, 6])
         self.len_data = len(self.data_set.index)
         self.num_batch = math.ceil(self.len_data / self.batch_size)
         self.arr = range(self.num_batch)
+        self.n_class = n_class
 
 
 
---
+++ utils/OpenPTrackGenerator.py
@@ -0,0 +1,59 @@
+from utils.Generator import Generator
+import os
+import pickle
+import tensorflow as tf
+from OpenPTrack.SkeletonPreProcessing import SkeletonPreProcessing
+import numpy as np
+
+class OpenPTrackGenerator(Generator):
+
+    def __init__(self, batch_size, dataset_path, skeleton_path, t=0, n_class=0, train=False, average=False):
+        '''
+             :param batch_size:
+             :param dataset_path:
+             :param skeleton_path:
+             :param t:
+             :param n_class:
+             :param train:
+             :param dt_type: data type b= berkeley
+        '''
+        Generator.__init__(self, batch_size=batch_size, dataset_path=dataset_path, t=t, n_class=n_class, train=train)
+
+        self.skeleton_path = skeleton_path
+        self.average = average
+        self.preprocessing = SkeletonPreProcessing()
+
+    def openSkeleton(self,subject, filename):
+        filepath = os.path.join(self.skeleton_path+subject, str(filename)+".pkl")
+        pickle_in = open(filepath, "rb")
+        skeleton = pickle.load(pickle_in)
+        skeleton.joints.normalize()
+        #print(skeleton.getFlattenCoordinates())
+        return skeleton.getFlattenCoordinates()
+    def getFlow(self, batch_index):
+        if (batch_index + 1) * self.batch_size > len(self.data_set.index):
+            data = self.data_set[self.len_data - self.batch_size: self.len_data]
+        else:
+            data = self.data_set[batch_index * self.batch_size: (batch_index + 1) * self.batch_size]
+
+        x = []
+        labels = []
+        weights = []
+        for index, row in data.iterrows():
+
+            skeletons = np.array([self.openSkeleton(subject=row.subject, filename=filename) for filename in range(row.time_min, row.time_max)])
+            skeletons = self.preprocessing.smoothing(skeletons)
+            x.append(tf.convert_to_tensor(skeletons, dtype=tf.float32))
+            labels.append(row["class"])
+            weights.append(row["weight"])
+
+        x = tf.stack(x, axis=0)
+        y = np.array(labels).flatten()
+        w = np.array(weights).flatten()
+
+        if self.average:
+            y = tf.reduce_mean(tf.reshape(tf.convert_to_tensor(self.lb.transform(y).astype(float), dtype=tf.float32), shape=(self.batch_size, self.T, self.n_class)), axis=1)
+        else:
+            y = tf.reshape(tf.convert_to_tensor(self.lb.transform(y).astype(float), dtype=tf.float32), shape=(self.batch_size, self.T, self.n_class))
+        w = tf.reshape(tf.convert_to_tensor(w, dtype=tf.float32), shape=(self.batch_size, self.T))
+        return x, y, w
